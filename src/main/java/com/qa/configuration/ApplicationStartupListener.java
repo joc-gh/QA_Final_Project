fcb494d036937d9ff36e344bcc53a89e24ab25ec
joc-gh <josh.cannon@gmail.com>
2022-02-01 12:33:36 +0000

    ApplicationStartupListener

--- src/main/java/com/qa/configuration/ApplicationStartupListener.java
+++ src/main/java/com/qa/configuration/ApplicationStartupListener.java
@@ -1,5 +1,33 @@
 package com.qa.configuration;
 
-public class ApplicationStartupListener {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.qa.data.entity.Spell;
+import com.qa.data.repository.SpellRepository;
+
+@Profile("dev")
+@Configuration
+public class ApplicationStartupListener implements ApplicationListener<ApplicationReadyEvent> {
+
+	private SpellRepository spellRepository;
+
+	@Autowired // dependency injection
+	public ApplicationStartupListener(SpellRepository spellRepository) {
+		this.spellRepository = spellRepository;
+	}
+
+	// The application has booted and its components are ready to server
+	// content when this method fires
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		spellRepository.saveAll(List.of(new Spell("Dimension Door", 4, "Conjuration"),
+				new Spell("Booming Blade", 0, "Evocation"), new Spell("Foresight", 9, "Divination")));
+	}
 
 }
